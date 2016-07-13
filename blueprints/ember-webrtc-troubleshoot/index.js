6694e07f7bd56bf3421b94932de7bbaf022c65f5
Xander Dumaine <xander.dumaine@inin.com>
2016-07-13 10:46:01 -0400

    remove unneeded deps

--- blueprints/ember-webrtc-troubleshoot/index.js
+++ blueprints/ember-webrtc-troubleshoot/index.js
@@ -3,14 +3,10 @@ module.exports = {
   normalizeEntityName: function () {},
 
   afterInstall: function () {
-    return this.addBowerPackageToProject('webrtc-adapter#gh-pages').then(() => {
-      return this.addBowerPackageToProject('cheet.js#0.3.3');
+    return this.addBowerPackageToProject('webrtc-adapter' 'gh-pages').then(() => {
+      return this.addBowerPackageToProject('cheet.js', '0.3.3');
     }).then(() => {
-      return this.addBowerPackageToProject('xdumaine/localmedia');
-    }).then(() => {
-      return this.addBowerPackageToProject('otalk/RTCPeerConnection');
-    }).then(() => {
-      return this.addBowerPackageToProject('webrtc-troubleshooter#0.1.5');
-    })
+      return this.addBowerPackageToProject('webrtc-troubleshooter', '0.1.5');
+    });
   }
 };
