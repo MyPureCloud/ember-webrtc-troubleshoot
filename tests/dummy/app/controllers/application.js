05f20738c8da4a0988e711c468bbb726a29eb8ad
CBrewski <iamsparticus33@gmail.com>
2018-07-18 09:26:06 -0600

    added some org numbers

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -6,12 +6,12 @@ export default Ember.Controller.extend({
   realtimeEnvironments: {
     'apps.mypurecloud.com':    { host: 'https://realtime.mypurecloud.com:443', orgId: 397 },
     'apps.mypurecloud.com.au': { host: 'https://realtime.mypurecloud.com.au:443', orgId: 397 }, // Need an orgId for this one
-    'apps.mypurecloud.de ':    { host: 'https://realtime.mypurecloud.de :443', orgId: 397 }, // Need an orgId for this one
-    'apps.mypurecloud.ie ':    { host: 'https://realtime.mypurecloud.ie :443', orgId: 397 }, // Need an orgId for this one
+    'apps.mypurecloud.de':     { host: 'https://realtime.mypurecloud.de:443', orgId: 397 }, // Need an orgId for this one
+    'apps.mypurecloud.ie':     { host: 'https://realtime.mypurecloud.ie:443', orgId: 397 }, // Need an orgId for this one
     'apps.mypurecloud.jp':     { host: 'https://realtime.mypurecloud.jp:443', orgId: 397 }, // Need an orgId for this one
-    'apps.inintca.com':        { host: 'https://realtime.inintca.com:443', orgId: 397 }, // Need an orgId for this one
-    'apps.inindca.com':        { host: 'https://realtime.inindca.com:443', orgId: 397 }, // Need an orgId for this one
-    'localhost':               { host: 'https://realtime.mypurecloud.com:443', orgId: 397 },
+    'apps.inintca.com':        { host: 'https://realtime.inintca.com:443', orgId: 1388 },
+    'apps.inindca.com':        { host: 'https://realtime.inindca.com:443', orgId: 1111 },
+    'localhost':               { host: 'https://realtime.inindca.com:443', orgId: 1111 },
   },
 
   init () {
@@ -41,13 +41,12 @@ export default Ember.Controller.extend({
 
   getRealtimeInstance () {
     const windowHost = window.location.host;
-    // const windowHost = "apps.mypurecloud.com";
-    // const windowHost = "apps.mypurecloud.com.au";
-    // const windowHost = "apps.mypurecloud.de ";
-    // const windowHost = "apps.mypurecloud.ie ";
-    // const windowHost = "apps.mypurecloud.jp";
-    // const windowHost = "apps.inintca.com";
-    // const windowHost = "apps.inindca.com";
+
+    // const windowHost = "apps.mypurecloud.com.au"; // fail
+    // const windowHost = "apps.mypurecloud.de"; // fail
+    // const windowHost = "apps.mypurecloud.ie"; // fail
+    // const windowHost = "apps.mypurecloud.jp"; // fail
+
     let host;
     let orgId;
     let environments = this.get('realtimeEnvironments');
