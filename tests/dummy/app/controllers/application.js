22e8408d93a653d28144b913964017ac7221b216
Chris Brewer <chris.brewer@genesys.com>
2018-07-19 08:48:33 -0600

    Abstracted environments to separate file

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,18 +1,8 @@
 import Ember from 'ember';
+import realtimeEnvironments from '../models/realtimeEnvironments';
 
 export default Ember.Controller.extend({
   iceServers: null,
-
-  realtimeEnvironments: {
-    'apps.mypurecloud.com':    { host: 'https://realtime.mypurecloud.com:443', orgId: 397 },
-    'apps.mypurecloud.com.au': { host: 'https://realtime.mypurecloud.com.au:443', orgId: 28 }, // Need an orgId for this one
-    'apps.mypurecloud.de':     { host: 'https://realtime.mypurecloud.de:443', orgId: 397 }, // Need an orgId for this one
-    'apps.mypurecloud.ie':     { host: 'https://realtime.mypurecloud.ie:443', orgId: 397 }, // Need an orgId for this one
-    'apps.mypurecloud.jp':     { host: 'https://realtime.mypurecloud.jp:443', orgId: 397 }, // Need an orgId for this one
-    'apps.inintca.com':        { host: 'https://realtime.inintca.com:443', orgId: 1388 },
-    'apps.inindca.com':        { host: 'https://realtime.inindca.com:443', orgId: 1111 },
-    'localhost':               { host: 'https://realtime.inindca.com:443', orgId: 1111 },
-  },
 
   init () {
     if (window.Realtime) {
@@ -40,28 +30,22 @@ export default Ember.Controller.extend({
   },
 
   getRealtimeInstance () {
-    // const windowHost = window.location.host;
-
-    const windowHost = "apps.mypurecloud.com.au"; // fail
-    // const windowHost = "apps.mypurecloud.de"; // fail
-    // const windowHost = "apps.mypurecloud.ie"; // fail
-    // const windowHost = "apps.mypurecloud.jp"; // fail
+    const windowHost = window.location.host;
 
     let host;
-    let orgId;
-    let environments = this.get('realtimeEnvironments');
+    let thirdPartyOrgId;
     if (windowHost.indexOf('localhost') > -1) {
-      host = environments.localhost.host;
-      orgId = environments.localhost.orgId;
+      host = realtimeEnvironments.localhost.host;
+      thirdPartyOrgId = realtimeEnvironments.localhost.thirdPartyOrgId;
     } else {
-      host = environments[windowHost].host;
-      orgId = environments[windowHost].orgId;
+      host = realtimeEnvironments[windowHost].host;
+      thirdPartyOrgId = realtimeEnvironments[windowHost].thirdPartyOrgId;
     }
 
     const realtime = new window.Realtime({
       host: host,
       guest: true,
-      orgId: orgId,
+      orgId: thirdPartyOrgId,
       jidRouting: true,
       jidResource: 'webrtc-troubleshoot'
     });
