f981bac9b109905c126f14ed9ec6aaf51b2aaf27
David Houseknecht <david.j.houseknecht@gmail.com>
2021-02-22 15:31:12 -0500

    PCM-1536 attempting to fix url bug and adding debugging

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -9,7 +9,7 @@ export default Ember.Route.extend({
   beforeModel (/* transition */) {
     return this.get('application').authenticate()
       .then(authData => {
-        this.potentiallyRedirectRoute(authData.state);
+        // this.potentiallyRedirectRoute(authData.state);
       })
       .then(() => this.get('intl').setLocale('en-us'));
   },
@@ -32,40 +32,64 @@ export default Ember.Route.extend({
       reload
   */
   potentiallyRedirectRoute (stateUrl) {
+    console.debug('potentiallyRedirectRoute() with stateUrl: ' + stateUrl);
     if (!stateUrl || stateUrl === '/') return;
 
+    const redirectRoute = authConfig.redirectUrl + stateUrl;
+
+    if (redirectRoute === window.location.href) {
+      return console.debug('redirectRoute matches current href: ' + redirectRoute);
+    }
+
+    console.debug('redirecting to: ' + redirectRoute);
+    return window.location.replace(redirectRoute);
+
     /* if we have a tm build and a hash */
-    const [partialPathname, hash] = stateUrl.split('#');
+    // let [partialPathname, hash] = stateUrl.split('#');
 
-    /* if there wasn't a hash, it means we don't have a TM version */
-    if (!hash) {
-      try {
-        return this.transitionTo(partialPathname);
-      } catch (error) {
-        /* no-op: this throws if partialPathname is a tm but there is no hash */
-      }
-    }
+    // console.debug('partialPathname: ' + partialPathname + '\nhash: ' + hash);
 
-    const currentRoute = window.location.origin + window.location.pathname;
-    const timemachineRoute = authConfig.redirectUrl + partialPathname;
+    // /* if there wasn't a hash, it means we don't have a TM version */
+    // if (!hash) {
+    //   try {
+    //     console.debug('attempting to transition to: ' + partialPathname);
+    //     return this.transitionTo(`${partialPathname}`);
+    //   } catch (error) {
+    //     console.debug(`error transitioning to: ${partialPathname} \n\t${error.message}`);
+    //     /* no-op: this throws if partialPathname is a tm but there is no hash */
+    //   }
+    // }
 
-    /* if we are already on the tm route */
-    if (currentRoute === timemachineRoute) {
-      /* if there is no hash, then we don't need to route to anything */
-      if (!hash) {
-        return null;
-      }
-      try {
-        /* if we have a hash, transition to it */
-        this.transitionTo(hash);
-      } catch (error) {
-        /* if our hash is the accessToken from auth redirect, this will throw – don't redirect */
-        return null;
-      }
-    }
+    // const currentRoute = window.location.origin + window.location.pathname;
+    // const timemachineRoute = authConfig.redirectUrl + partialPathname;
 
-    /* otherwise, we need to switch to the tm route */
-    const fullTmHref = timemachineRoute + (hash || '');
-    window.location.replace(fullTmHref);
+    // /* add `#` back in for nested Ember routes */
+    // hash = hash ? `#${hash}` : null;
+
+    // console.debug('currentRoute: ' + currentRoute + '\ntimemachineRoute: ' + timemachineRoute + '\nhash: ' + hash);
+
+    // /* if we are already on the tm route */
+    // if (currentRoute === timemachineRoute) {
+    //   /* if there is no hash, then we don't need to route to anything */
+    //   if (!hash) {
+    //     return null;
+    //   }
+
+    //   try {
+    //     /* if we have a hash, transition to it */
+    //     console.debug('attempting to transition to: ' + hash);
+    //     this.transitionTo(hash);
+    //   } catch (error) {
+    //     /* if our hash is the accessToken from auth redirect, this will throw – don't redirect */
+    //     console.debug(`error transitioning to: ${hash} \n\t${error.message}`);
+    //     return null;
+    //   }
+    // }
+
+    // /* otherwise, we need to switch to the tm route */
+    // const fullTmHref = timemachineRoute + (hash ? `#${hash}` : '');
+    // console.debug('replacing url: ' + fullTmHref);
+
+    // window.location.replace(fullTmHref);
   }
 });
--- tests/dummy/app/services/application.js
+++ tests/dummy/app/services/application.js
@@ -24,13 +24,13 @@ export default Ember.Service.extend({
     }
 
     const { clientId, redirectUrl } = authConfig;
-    const redirectState = this._buildRedirectState();
+    // const redirectState = this._buildRedirectState();
 
     try {
       const authData = await this.get('authClient').loginImplicitGrant(
         clientId,
-        redirectUrl,
-        { state: redirectState }
+        redirectUrl
+        // { state: redirectState }
       );
       this._setAuthData(authData);
       return authData;
